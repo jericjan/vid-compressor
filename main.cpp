81f1f4d399f7ac444d6cbf8afd03a65e9eee3207
jericjan <cantilfrederick@gmail.com>
2024-05-31 23:45:55 +0800

    audio and pass options

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream> // cin & cout
-#include <cstdlib> // system
-#include <cmath> // std::ceil
+#include <cstdlib>  // system
+#include <cmath>    // std::ceil
 
-#include <cstdio> // std::remove
-#include <memory> //std::unique_ptr
+#include <cstdio>    // std::remove
+#include <memory>    //std::unique_ptr
 #include <stdexcept> //std::runtime_error
-#include <string> //std::string
-#include <array> //std::array
+#include <string>    //std::string
+#include <array>     //std::array
 
 #include <algorithm> //std::remove()
-#include <vector> //std::vector
+#include <vector>    //std::vector
+
+std::string input;
+int targetBitrate;
+bool includeAudio;
 
 double exec(const char *cmd)
 {
@@ -54,32 +58,12 @@ void deleteFiles(std::vector<const char *> filenames)
     }
 }
 
-int main()
+void two_pass()
 {
-    int targetSize, vidLength, targetBitrate;
-    std::string input;
-
-    std::cout << "Enter target MiB size: ";
-    std::cin >> targetSize;
-
-    std::cout << "Enter input filename: ";    
-    std::cin.ignore();
-    std::getline(std::cin, input);
-
-    std::string lenCmd0 = ("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"" + input + "\"").c_str();
-    const char *lenCmd = lenCmd0.c_str();
-
-    double vidLenLong = exec(lenCmd);
-    vidLength = std::ceil(vidLenLong);
-    std::cout << "Vid length is " << std::to_string(vidLength) << " seconds\n";
-
-    targetBitrate = (targetSize * 8388.608 / vidLength) - 128;
-
-    std::cout << "target bit rate is " << targetBitrate << "kBit/s" << std::endl;
-
-    std::cout << "Compressing into x264 MP4...\n";
     std::string command1 = "ffmpeg -y -i \"" + input + "\" -c:v libx264 -b:v " + std::to_string(targetBitrate) + "k -pass 1 -an -f null NUL -loglevel error";
-    std::string command2 = "ffmpeg -y -i \"" + input + "\" -c:v libx264 -b:v " + std::to_string(targetBitrate) + "k -pass 2 -c:a aac -b:a 128k \"" + input + "-compressed.mp4\" -loglevel error";
+
+    std::string audioFlag = includeAudio ? "-b:a 128k" : "-an";
+    std::string command2 = "ffmpeg -y -i \"" + input + "\" -c:v libx264 -b:v " + std::to_string(targetBitrate) + "k -pass 2 -c:a aac " + audioFlag + " \"" + input + "-compressed.mp4\" -loglevel error";
 
     // Convert to const char* when using in system()
     const char *cmd1 = command1.c_str();
@@ -101,6 +85,95 @@ int main()
     }
 
     deleteFiles({"ffmpeg2pass-0.log", "ffmpeg2pass-0.log.mbtree"});
+}
+
+void one_pass()
+{
+    std::string audioFlag = includeAudio ? "-b:a 128k" : "-an";
+    std::string command1 = "ffmpeg -y -i \"" + input + "\" -c:v libx264 -b:v " + std::to_string(targetBitrate) + "k " + audioFlag + " \"" + input + "-compressed.mp4\" -loglevel error";
+    const char *cmd1 = command1.c_str();
+    int result1 = system(cmd1);
+
+    if (result1 == 0)
+    {
+        std::cout << "FFmpeg commands executed successfully." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Error running FFmpeg commands." << std::endl;
+    }
+}
+
+void selectMode()
+{
+
+    int passMode;
+    std::cout << "Select a mode:\n[1] One-pass\n[2] Two-pass\n";
+    std::cin >> passMode;
+    switch (passMode)
+    {
+    case 1:
+        one_pass();
+        break;
+    case 2:
+        two_pass();
+        break;
+    default:
+        std::cout << "That's not in the options!";
+        selectMode();
+    }
+}
+
+void setAudio()
+{
+    char choice;
+    std::cout << "Include audio? (y/n) ";
+    std::cin >> choice;
+    switch (choice)
+    {
+    case 'y':
+        includeAudio = true;
+        break;
+    case 'n':
+        includeAudio = false;
+        break;
+    default:
+        std::cout << "That's not in the options!";
+        setAudio();
+    }
+}
+
+int main()
+{
+    int targetSize, vidLength;
+
+    std::cout << "Enter target MiB size: ";
+    std::cin >> targetSize;
+
+    std::cout << "Enter input filename: ";
+    std::cin.ignore();
+    std::getline(std::cin, input);
+
+    std::string lenCmd0 = ("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"" + input + "\"").c_str();
+    const char *lenCmd = lenCmd0.c_str();
+
+    double vidLenLong = exec(lenCmd);
+    vidLength = std::ceil(vidLenLong);
+    std::cout << "Vid length is " << std::to_string(vidLength) << " seconds\n";
+
+    setAudio();
+
+    targetBitrate = (targetSize * 8388.608 / vidLength);
+
+    if (includeAudio == true)
+    {
+        targetBitrate -= 128;
+    }
+
+    std::cout << "target bit rate is " << targetBitrate << "kBit/s" << std::endl;
+    std::cout << "This will be converted into x264 MP4\n";
+
+    selectMode();
 
     return 0;
 }
